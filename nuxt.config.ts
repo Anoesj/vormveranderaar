3e87c73d38e235d8d49e9661aa7538066e3c9501
Anoesj <anoesjsadraee@gmail.com>
2024-09-12 11:42:51 +0200

    Fix bundling

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,4 +34,17 @@ export default defineNuxtConfig({
       calculateInBrowserOnly: false,
     },
   },
+  // NOTE: Workaround for auto imports & bundling & web workers.
+  // See: https://github.com/nuxt/nuxt/issues/24590#issuecomment-2318843660
+  hooks: {
+    'vite:extendConfig' (viteInlineConfig, env) {
+      if (env.isClient) {
+        const importPlugin = viteInlineConfig.plugins?.find(p => p && 'name' in p && p.name === 'nuxt:imports-transform')
+        if (importPlugin) {
+          viteInlineConfig.worker ||= {}
+          viteInlineConfig.worker.plugins = () => [importPlugin]
+        }
+      }
+    },
+  },
 });
