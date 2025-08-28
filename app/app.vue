017f3295f732e4cec7d41168d6c49c28cc046cac
Anoesj <anoesjsadraee@gmail.com>
2025-08-28 12:45:28 +0200

    Fix wrongfully disabled buttons

--- app/app.vue
+++ app/app.vue
@@ -119,43 +119,49 @@
         <CardHeader class="flex flex-row items-start justify-between gap-4">
           <div class="flex gap-4">
             <div class="grid grid-cols-[auto_1fr] items-center gap-x-2 gap-y-4">
-              <Switch
-                id="show-figures"
-                v-model:checked="showFigures"
-              />
-              <Label for="show-figures" class="leading-5">
-                Show original figures on game boards (if available)
-                <br>
-                <span class="text-gray-400">
-                  If turned off, a numeric representation will be shown.
-                </span>
-              </Label>
+              <div class="grid grid-cols-subgrid col-span-full items-center">
+                <Switch
+                  id="show-figures"
+                  v-model:checked="showFigures"
+                />
+                <Label for="show-figures" class="leading-5">
+                  Show original figures on game boards (if available)
+                  <br>
+                  <span class="text-gray-400">
+                    If turned off, a numeric representation will be shown.
+                  </span>
+                </Label>
+              </div>
 
-              <Switch
-                id="calculate-in-browser"
-                v-model:checked="calculateInBrowser"
-                :disabled="runtimeConfig.public.calculateInBrowserOnly"
-              />
-              <Label for="calculate-in-browser" class="leading-5">
-                Calculate in-browser
-                <br>
-                <span class="text-gray-400">
-                  This will run the calculation in your browser instead of in the Bun-powered server.
-                </span>
-              </Label>
+              <div class="grid grid-cols-subgrid col-span-full items-center">
+                <Switch
+                  id="calculate-in-browser"
+                  v-model:checked="calculateInBrowser"
+                  :disabled="runtimeConfig.public.calculateInBrowserOnly"
+                />
+                <Label for="calculate-in-browser" class="leading-5">
+                  Calculate in-browser
+                  <br>
+                  <span class="text-gray-400">
+                    This will run the calculation in your browser instead of in the Bun-powered server.
+                  </span>
+                </Label>
+              </div>
 
-              <Switch
-                id="prepare-possible-solution-starts"
-                v-model:checked="preparePossibleSolutionStarts"
-              />
-              <Label for="prepare-possible-solution-starts" class="leading-5">
-                Prepare possible solution starts
-                <br>
-                <span class="text-gray-400">
-                  When enabled, we won't just brute force the puzzle, but prepare some
-                  possible solution starts based on correct corner outputs.
-                </span>
-              </Label>
+              <div class="grid grid-cols-subgrid col-span-full items-center">
+                <Switch
+                  id="prepare-possible-solution-starts"
+                  v-model:checked="preparePossibleSolutionStarts"
+                />
+                <Label for="prepare-possible-solution-starts" class="leading-5">
+                  Prepare possible solution starts
+                  <br>
+                  <span class="text-gray-400">
+                    When enabled, we won't just brute force the puzzle, but prepare some
+                    possible solution starts based on correct corner outputs.
+                  </span>
+                </Label>
+              </div>
             </div>
           </div>
 
